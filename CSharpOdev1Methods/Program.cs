b54056898c7e523d8201b6b65d13ed4d8a63fbf8
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-01-10 10:45:09 +0300

    CSharpVKIHesaplaMethods

--- CSharpOdev1Methods/Program.cs
+++ CSharpOdev1Methods/Program.cs
@@ -2,55 +2,81 @@
 {
     public class Program
     {
+        
+
         public static void Main()
         {
-            Menu();
             
-            
+                TeshisYaz();
+           
         }
-
-        static void Menu()
+ 
+        static (double boy, double kilo) BoyKiloAl()
         {
             Console.Clear();
             Console.WriteLine("Merhaba, lütfen vücut kitle endeksinizin belirlenmesi için boy ve kilo değerlerinizi giriniz.");
-            TeshisYaz();
-            
-        }
-        static (double boy, double kilo) BoyKiloAl()
-        {
             Console.Write("Boy (m) : ");
             double boy = Convert.ToDouble(Console.ReadLine());
             Console.Write("Kilo (kg) : ");
             double kilo = Convert.ToDouble(Console.ReadLine());
             return (boy, kilo);
         }
-        static double VKIHesapla()
-        {
-            var veriler = BoyKiloAl();
-            double VKI = veriler.kilo / (veriler.boy * veriler.boy);
+        static double VKIHesapla(double boy,double kilo)
+        { 
+            double VKI = kilo / (boy * boy);
             return VKI;
         }
         static void TeshisYaz()
         {
-            var VKI = VKIHesapla();
             var veriler = BoyKiloAl();
-
+            double VKI = VKIHesapla(veriler.boy,veriler.kilo);
+            string teshis = TeshisKoy(VKI);
+            Console.WriteLine($"Boy : {veriler.boy} , Kilo  :{veriler.kilo}, VKI : {VKI} Teşhis : {teshis}");
+            DevamMi();
+        }
+        static string TeshisKoy(double VKI)
+        {
             if (VKI < 18.49)
             {
-                Console.WriteLine($"Hastanın; boyu: {veriler.boy}, kilosu: {veriler.kilo}, VKI indeksi: {VKI}, Teşhis: Zayıf Hasta");
+                return "Zayıf";
             }
             else if (VKI >= 18.49 || VKI < 24.99)
             {
-                Console.WriteLine($"Hastanın; boyu: {veriler.boy}, kilosu: {veriler.kilo}, VKI indeksi: {VKI}, Teşhis: İdeal Kiloda Hasta");
+                return "İdeal Kiloda";
             }
             else if (VKI >= 24.99 || VKI < 29.99)
             {
-                Console.WriteLine($"Hastanın; boyu: {veriler.boy}, kilosu: {veriler.kilo}, VKI indeksi: {VKI}, Teşhis: Hafif Kilolu Hasta");
+                return "Hafif Kilolu";
             }
             else
             {
-                Console.WriteLine($"Hastanın; boyu: {veriler.boy}, kilosu: {veriler.kilo}, VKI indeksi: {VKI}, Teşhis: Obez Hasta");
+                return "Obez";
+            }
+        }
+        static void DevamMi()
+        {
+            Console.Write("Teşhisiniz koyulmuştur.Devam edip başka işlem yapmak istiyor musunuz ? ( E/H)");
+            string cevap = Console.ReadLine();
+
+            switch (cevap)
+            {
+
+                case "E":
+                case "e":
+                    TeshisYaz();
+                    break;
+                case "H":
+                case "h":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Hatalı bir seçim yaptınız!! Lütfen geçerli bir cevap giriniz,tekrar denemek için ENTER (E/H)");
+                    Console.ReadLine();
+                    DevamMi();
+                    break;
+
             }
         }
     }
+
 }
